281584065963d2c61a2947d300a1df765236299a
tetraPRO <caputo.philip@gmail.com>
2018-03-01 14:15:09 -0700

    another column

--- grails-app/domain/tetrapro_financial/Budget.groovy
+++ grails-app/domain/tetrapro_financial/Budget.groovy
@@ -15,6 +15,7 @@ class Budget {
   Double medical
   Double fees
   Double education
+  Double other
   Double futureself
 
     static constraints = {
@@ -31,6 +32,7 @@ class Budget {
       medical()
       fees()
       education()
+      other()
       futureself()
     }
 }
