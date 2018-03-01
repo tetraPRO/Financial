1dd6a0decb7a36525375f77ea7b8fc2ef70f7db8
tetraPRO <caputo.philip@gmail.com>
2018-03-01 14:12:49 -0700

    added domain-class properties and DB columns

--- grails-app/domain/tetrapro_financial/Budget.groovy
+++ grails-app/domain/tetrapro_financial/Budget.groovy
@@ -2,6 +2,35 @@ package tetrapro_financial
 
 class Budget {
 
+  Double rentmortgage
+  Double utilities
+  Double cellphones
+  Double vehicle
+  Double insurance
+  Double gas
+  Double creditcards
+  Double entertainment
+  Double foodgroceries
+  Double clothing
+  Double medical
+  Double fees
+  Double education
+  Double futureself
+
     static constraints = {
+      rentmortgage()
+      utilities()
+      cellphones()
+      vehicle()
+      insurance()
+      gas()
+      creditcards()
+      entertainment()
+      foodgroceries()
+      clothing()
+      medical()
+      fees()
+      education()
+      futureself()
     }
 }
